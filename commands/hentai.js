a66ba1570a8528b2cb80f7045d3ff2016af3534d
pamosa <69703617+pamosa@users.noreply.github.com>
2022-01-11 13:55:06 -0300

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -6,29 +6,28 @@ const discord = require('discord.js')
 const discordnsfw = require('@jcauman23/discordnsfw')
 const NSFW = new discordnsfw()
 const hentaiTypes = [
-  ['anal', 'anal'],
-  ['4k', 'fourk'],
-  ['ass', 'ass'],
-  ['gonewild', 'gonewild'],
-  ['porngif', 'pgif'],
-  ['pussy', 'pussy'],
-  ['thigh', 'pussy'],
-  ['boobs', 'boobs'],
+  ['wallpaper', 'wallpaper'],
+  ['solo', 'solo'],
+  ['lewd', 'lewd'],
+  ['kitsune', 'kitsune'],
+  ['erokemo', 'erokemo'],
+  ['hentaimidriff', 'hmidriff'],
   ['hentaiass', 'hass'],
   ['hentai', 'hentai'],
   ['hentaithigh', 'hthigh'],
   ['erokemo', 'erokemo'],
   ['neko', 'neko'],
-  ['cum', 'cum'],
+  ['nekogif', 'nekogif'],
+  ['nekofeet', 'nekofeet'],
+  ['nekopussy', 'nekopussy'],
+  ['nekotits', 'nekotits'],
   ['yuri', 'yuri']
 ]
 
 function validateHentaiType(hentaiType) {
   let v
   hentaiTypes.forEach(ht => {
-    console.log(ht[0], hentaiType)
     if(ht[0] == hentaiType) {
-      console.log('yes')
       v = ht
     }
   }) 
@@ -36,13 +35,12 @@ function validateHentaiType(hentaiType) {
   return v
 }
 
-exports.classe = 'Diversão'
+exports.classe = '+18'
 exports.desc = 'Hentai aleatório'
 exports.run = async(client, message, args) => {
   if(!message.channel.nsfw) {message.channel.send('calma aí meu punheteiro, o canal precisa ser **NSFW** para enviar hentais!'); return}
   
   var htype = args[1]
-  console.log(htype)
   
   if(!htype) {
     message.channel.send('meu querido, especifica o CARALHO do tipo de hentai filho de uma puta!')
@@ -58,7 +56,10 @@ exports.run = async(client, message, args) => {
   
   h = h[1]
   
-  message.channel.send(NSFW[h]()).then(() => {}).catch(() => {
+  var hentai_img = await NSFW[h]()
+  
+  message.channel.send(hentai_img).then(() => {}).catch(err => {
+    console.log(err)
     message.channel.send('algum problema deu ao enviar o hentai, tente novamente meu amigo!')
   })
 }
